3a5bd6aeed4fe777a644b0ec303b3dda390eaa78
oaleksander <medvedev.asm@outlook.com>
2020-10-02 16:11:44 +0300

    Rearranged math functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/math/MathUtil.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.math;
+
+import static java.lang.Math.PI;
+
+public class MathUtil {
+    public static final double EPSILON = 1e-6;
+
+    public static boolean approxEquals(double d1, double d2) {
+        if (Double.isInfinite(d1)) {
+            // Infinity - infinity is NaN, so we need a special case
+            return d1 == d2;
+        } else {
+            return Math.abs(d1 - d2) < EPSILON;
+        }
+    }
+
+    public static double angleTransform(double angle)
+    {
+        while (angle>PI) angle -= PI*2;
+        while (angle<-PI) angle += PI*2;
+        return angle;
+    }
+
+    public static double cosFromSin(double sin, double angle)
+    {
+        double cos = Math.sqrt(1-sin*sin);
+        angle = angle % (PI*2);
+        if ((angle>PI) && (angle<PI*2))
+            cos *= -1;
+        return cos;
+    }
+}
