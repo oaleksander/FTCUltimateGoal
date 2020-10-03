d86196fb354f5e3de6fb1527fe6305a8d9ba78ba
oaleksander <medvedev.asm@outlook.com>
2020-10-03 13:33:49 +0300

    Trying to refactor WoENrobot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tele_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tele_test.java
@@ -1,17 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.robot.WoENrobot;
 
-@TeleOp(name="Teleop TEST/PRACTICE", group="")
-public class Tele_test extends WoENrobot {
+@TeleOp(name="Teleop TEST/PRACTICE", group="Debugging")
+public class Tele_test extends LinearOpMode {
     @Override
     public void runOpMode() {
 
         //initRobot();
-        forceInitRobot();
-        startRobot();
+        WoENrobot.getInstance().forceInitRobot(this);
+        WoENrobot.getInstance().startRobot();
         //odometry.start();
         while(opModeIsActive())
         {
@@ -20,12 +21,12 @@ public class Tele_test extends WoENrobot {
             else turn -= gamepad1.left_trigger;
             if(gamepad1.right_bumper) turn +=0.25;
             else turn += gamepad1.right_trigger;
-            omniMoveYX(-gamepad1.left_stick_y, gamepad1.left_stick_x, turn);
+            WoENrobot.drivetrain.holonomicMove(-gamepad1.left_stick_y, gamepad1.left_stick_x, turn);
             double f = gamepad1.right_stick_y;
             telemetry.addData("Status", "Running");
-            telemetry.addData("Y", odometry.getRobotCoordinates().y);
-            telemetry.addData("X", odometry.getRobotCoordinates().x);
-            telemetry.addData("Heading", Math.toDegrees(odometry.getRobotCoordinates().heading));
+            telemetry.addData("Y",  WoENrobot.odometry.getRobotCoordinates().y);
+            telemetry.addData("X",  WoENrobot.odometry.getRobotCoordinates().x);
+            telemetry.addData("Heading", Math.toDegrees( WoENrobot.odometry.getRobotCoordinates().heading));
             telemetry.update();
 
         }
