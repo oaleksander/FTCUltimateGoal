0d89050b00cc9af03c416c1a0d0d1caa3bbc6460
oaleksander <medvedev.asm@outlook.com>
2020-10-05 02:22:17 +0300

    Created a template for movement test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MovTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MovTest.java
@@ -3,22 +3,34 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.misc.AutoTransitioner;
+import org.firstinspires.ftc.teamcode.math.Pose2D;
 import org.firstinspires.ftc.teamcode.robot.WoENrobot;
 
-@Autonomous(name = "Do nothing auto (template)")
-public class NullAuto extends LinearOpMode {
+@Autonomous(name = "Movement test")
+public class MovTest extends LinearOpMode {
     @Override
     public void runOpMode() {
 
         WoENrobot.getInstance().forceInitRobot(this);
-        WoENrobot.getInstance().tFdetector.initialize();
+        WoENrobot.tFdetector.initialize();
         //AutoTransitioner.transitionOnStop(this, "Teleop COMPETITION");
-        WoENrobot.getInstance().tFdetectorThread.start();
+        WoENrobot.tFdetectorThread.start();
         WoENrobot.getInstance().startRobot();
-        WoENrobot.getInstance().tFdetector.retrieveResult();
+
 /*------------------------------------------------------------------------------------------------*/
-// AUTO CODE GOES HERE
+        switch(WoENrobot.tFdetector.retrieveResult())
+        {
+            case 4:
+                WoENrobot.drivetrain.Pos(new Pose2D(50,100,Math.toRadians(45)));
+                break;
+            case 1:
+                WoENrobot.drivetrain.Pos(new Pose2D(-50,80,Math.toRadians(-45)));
+                break;
+            default:
+                WoENrobot.drivetrain.Pos(new Pose2D(0,1,Math.toRadians(0)));
+                break;
+        }
+
 /*------------------------------------------------------------------------------------------------*/
         while(opModeIsActive())
         {
