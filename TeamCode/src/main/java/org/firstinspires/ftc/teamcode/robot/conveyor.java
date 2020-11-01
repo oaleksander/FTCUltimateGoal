d6508f1179f72152e5fbd7e259d1bda58852d914
smert-WoEN <72210151+smert-WoEN@users.noreply.github.com>
2020-11-01 19:47:29 +0300

    add conveyor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/conveyor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/conveyor.java
@@ -1,11 +1,87 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-public class conveyor {
+
+public class conveyor extends LinearOpMode {
     public DcMotorEx conveyor = null;
+    public Servo feeder = null;
     public ElapsedTime conveyortime = new ElapsedTime();
+    float[] hsvValues = new float[3];
+    ColorSensor colorSensor;
+    DistanceSensor sensorDistance;
+    /** main int */
+    public void initialize(){
+        initializecolor();
+        initializedrive();
+        initializedservo();
+    }
+    /** int color and distanse */
+    public void initializecolor(){
+        colorSensor = hardwareMap.get(ColorSensor.class, "color");
+        sensorDistance = hardwareMap.get(DistanceSensor.class, "color");
+    }
+    /** int motors */
+    public void initializedrive(){
+        conveyor = hardwareMap.get(DcMotorEx.class, "conveyor");
 
+        conveyor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        conveyor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        conveyor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    /** int servo */
+    public void initializedservo(){
+        feeder = hardwareMap.get(Servo.class, "feeder");
+    }
+    // правильная ли конвертация?
+    public float[] getcolor(){
+        int colors = colorSensor.argb();
+   /*     int a = Color.alpha(colors);
+        int r = Color.red(colors);
+        int g = Color.green(colors);
+        int b = Color.blue(colors);
+     */   Color.colorToHSV(colors, hsvValues);
+        return hsvValues;
+    }
+    public float maxcolor = 255, mincolor = 0;
+    public double getdistance(){
+        return sensorDistance.getDistance(DistanceUnit.CM);
+    }
+    boolean on = false;
+    public void update(){
+        if ((mincolor <= getcolor()[0]) && (getcolor()[0]<= maxcolor) && (conveyortime.milliseconds()>=1000)){
+            conveyortime.reset();
+        }
+        if (on && conveyortime.milliseconds()<=1000) {
+            if (conveyor.getCurrent(CurrentUnit.AMPS)<= 4){
+                setpowerconveyor(1);
+            }
+            else {
+                setpowerconveyor(-1);
+            }
+        }
+        else {
+            setpowerconveyor(0);
+        }
+    }
+    public void setpowerconveyor(double power){
+        conveyor.setPower(power);
+    }
+    public void runOpMode(){}
 }
