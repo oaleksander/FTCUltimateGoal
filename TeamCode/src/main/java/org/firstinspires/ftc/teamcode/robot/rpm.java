24e37916ac258d30e9eabd683c47e584e41e0cda
smert-WoEN <72210151+smert-WoEN@users.noreply.github.com>
2020-11-03 15:43:40 +0300

    experement rpm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/rpm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/rpm.java
@@ -12,7 +12,7 @@ public class rpm implements RobotModule {
 
     public static DcMotorEx shooterMotor = null;
     public static ElapsedTime rpmtime = new ElapsedTime();
-    private static LinearOpMode opMode;
+    public static LinearOpMode opMode;
 
     public void setOpMode(LinearOpMode opMode) {
         this.opMode = opMode;
@@ -29,16 +29,21 @@ public class rpm implements RobotModule {
     private static boolean isActivated;
     static double rpm = 6000;
     static boolean on = false;
-
+    static double rpm2 = 0;
     public void update() {
-        //rpmtime.reset();
-        //double x = 1;
+        posold = pos;
+        pos = shooterMotor.getCurrentPosition();
+        oldtime = time;
+        time = rpmtime.milliseconds();
+        posold -= pos;
+        oldtime -= time;
+        rpm2 = posold / oldtime * 2500; // что выводит?
         if (on) {
             if (time_ms > rpmtime.milliseconds()) {
                 shooterpower(rpmtime.milliseconds() * x);
             }
             if (time_ms < rpmtime.milliseconds()) {
-                shooterpower(1);
+                shooterpower(rpm / 6000);
             }
         }
         else {
@@ -52,12 +57,24 @@ public class rpm implements RobotModule {
         this.on = on;
         rpmtime.reset();
     }
+    double pos = 0, posold = 0,time = 0, oldtime = 0, error = 0, P = 0, D = 0, errorold = 0,power = 0;
+    static final double minerror = 15, maxspeed = 1, kofP = 0.0015, kofd = 0.00001;
+    public double regulator(){ // что выводит?
+        error = rpm - rpm2;
+        P = error * kofP;
+        D = (error - errorold)* kofd;
+        return power = D + P;
+    }
+    public void resetshooter(){
+        shooterMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        shooterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
     public void setrpm(double rpm){
         this.rpm = rpm;
     }
     public void setspeedlevel(double time){
         this.time_ms = time;
         if (time_ms > 0)
-            x = 1 / time_ms;
+            x = rpm / time_ms / 6000;
     }
 }
