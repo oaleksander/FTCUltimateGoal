4f8e60fd51608f052f72ceb75756d47863fcbb9b
oaleksander <medvedev.asm@outlook.com>
2020-11-07 13:05:43 +0300

    Optimized opencv camera closing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/callopencv.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/callopencv.java
@@ -1,13 +1,9 @@
 package org.firstinspires.ftc.teamcode.opencv;
 
-import android.widget.TextView;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
-import org.firstinspires.ftc.teamcode.robot.OpenCVNode;
 import org.firstinspires.ftc.teamcode.robot.WoENrobot;
 
 @TeleOp
@@ -20,7 +16,6 @@ public class callopencv extends LinearOpMode {
         WoENrobot.forceInitRobot(this);
         WoENrobot.openCVNode.initialize(this);
         waitForStart();
-        TextView tw = new TextView(AppUtil.getInstance().getActivity());
         while (opModeIsActive()) {
             telemetry.addData("mean", WoENrobot.openCVNode.getMean());
             telemetry.addData("getAspectRatio", WoENrobot.openCVNode.getAspectRatio());
