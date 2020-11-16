45fe4fcea871621a7ef9486d5cd1d8c821086958
smert-WoEN <denisdmit03@gmail.com>
2020-11-16 22:44:33 +0300

    WobbleManipulator remake, add WobbleManipulator2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/WobbleManipulator2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/WobbleManipulator2.java
@@ -1,37 +1,34 @@
-
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.superclasses.RobotModule;
-
-public class WobbleManipulator implements RobotModule {
-
+@Disabled
+public class WobbleManipulator2 {
     static final double minerror = 15, maxspeed = 0.7, kofP = 0.0015, kofd = 0.00001;
     public static DcMotorEx lever = null;
     public static Servo close = null;
-
+    // public static Servo angle = null;
     public boolean isGrabbed = true;
     public static ElapsedTime levertime = new ElapsedTime();
     static double pos = 0;
     static double power = 0, P = 0, D = 0, errorOld = 0, error = 0;
     private static LinearOpMode opMode = null;
-    double oldpower = 0;
+    double oldpower = 0, oldpos = 0;
 
     public void setOpMode(LinearOpMode opMode) {
-        WobbleManipulator.opMode = opMode;
+      //  WobbleManipulator.opMode = opMode;
     }
 
     public void initialize() {
 
         lever = opMode.hardwareMap.get(DcMotorEx.class, "lever");
         close = opMode.hardwareMap.get(Servo.class, "wobbleGrabber");
-
+        //   angle = opMode.hardwareMap.get(Servo.class, "angle");
 
         lever.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         lever.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -74,6 +71,7 @@ public class WobbleManipulator implements RobotModule {
             }
         }
     }
+    //double medium = 0.5, down = 0, up = 1, oldposangle = up;
     short posangle = 0;
     boolean ismed = false, isdown = false;
     public void upmediumdown(boolean upmedium, boolean updown){
@@ -82,11 +80,11 @@ public class WobbleManipulator implements RobotModule {
                 ismed = true;
                 if (posangle == 1){
                     posangle = 0;
-                    changepos(positions.up);
+                    changepos(WobbleManipulator.positions.up);
                 }
                 else {
                     posangle = 1;
-                    changepos(positions.medium);
+                    changepos(WobbleManipulator.positions.medium);
                 }
             }
         }
@@ -96,19 +94,47 @@ public class WobbleManipulator implements RobotModule {
                 isdown = true;
                 if(posangle == 2){
                     posangle = 0;
-                    changepos(positions.up);
+                    changepos(WobbleManipulator.positions.up);
                 }
                 else{
                     posangle = 2;
-                    changepos(positions.down);
+                    changepos(WobbleManipulator.positions.down);
                 }
             }
         }
         else isdown = false;
+        /*
+        if(upmedium != ismed){
+            ismed = upmedium;
+            isdown = false; // test
+            if (upmedium) changepos(positions.up);
+            else changepos(positions.medium);
+        }
+        if (updown != isdown){
+            isdown = updown;
+            ismed = false; //test
+            if(updown) changepos(positions.up);
+            else changepos(positions.down);
+        }*/
     }
-
+    /*
+    if(gamepad2.b)
+        {
+            if(pushback== false)
+            {
+                pushback = true;
+                if (speed == 1) {
+                    speed = 0.5;
+                } else {
+                    speed = 1;
+                }
+            }
+        }
+        else
+            pushback = false;
+     */
     enum positions {up, down, medium}
-    public void changepos(positions Positions){
+    public void changepos(WobbleManipulator.positions Positions){
         switch (Positions){
             case up:
                 setposlever(0);
@@ -123,8 +149,16 @@ public class WobbleManipulator implements RobotModule {
 
     }
 
+   /* public void setAngle(double posa){
+        if (posa != oldpos){
+            angle.setPosition(posa);
+            oldpos = posa;
+        }
+    }
+
+    */
+
     public void setposlever(double pos) {
         WobbleManipulator.pos = pos;
     }
-
 }
