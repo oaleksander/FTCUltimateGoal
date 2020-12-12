607ebb27734f70f1765b4ced0a1f0bc3b0b4f573
oaleksander <medvedev.asm@outlook.com>
2020-12-12 12:24:08 +0300

    MotionTask class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto_sandbox.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto_sandbox.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 
 import org.firstinspires.ftc.teamcode.math.Pose2D;
+import org.firstinspires.ftc.teamcode.superclasses.MotionTask;
 import org.openftc.revextensions2.ExpansionHubServo;
 
 import static org.firstinspires.ftc.teamcode.robot.WoENrobot.*;
@@ -15,6 +16,8 @@ public class Auto_sandbox extends AutonomousOpMode {
     public void main() {
         do {
             movement.Pos(new Pose2D(getStartPosition().x, 25, 0));
+            MotionTask mt = new MotionTask(0,0,0,()->{});
+            Pose2D p2d = (Pose2D)mt;
         } while (opModeIsActive());
     }
 }
