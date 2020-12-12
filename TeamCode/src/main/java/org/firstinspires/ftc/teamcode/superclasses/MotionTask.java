9c45d36a9b1eb63bfbb2264b5473c83b3cf4bb61
oaleksander <medvedev.asm@outlook.com>
2020-12-12 12:24:08 +0300

    MotionTask class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/superclasses/MotionTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/superclasses/MotionTask.java
@@ -1,26 +1,26 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.superclasses;
 
 import org.firstinspires.ftc.teamcode.math.Pose2D;
 
-public class motionTask extends Pose2D {
+public class MotionTask extends Pose2D {
 
 
     public ActionOnCompletion actionOnConpletion = () -> {};
-    public motionTask(double x, double y, double heading, ActionOnCompletion actionOncompletion) {
+    public MotionTask(double x, double y, double heading, ActionOnCompletion actionOncompletion) {
         super(x,y,heading);
         this.actionOnConpletion = actionOncompletion;
     }
 
-    public motionTask(double x, double y, double heading){
+    public MotionTask(double x, double y, double heading){
         super(x,y,heading);
     }
 
-    public motionTask(double x, double y, ActionOnCompletion actionOncompletion) {
+    public MotionTask(double x, double y, ActionOnCompletion actionOncompletion) {
         super(x,y,Double.NaN);
         this.actionOnConpletion = actionOncompletion;
     }
 
-    public motionTask(double x, double y) {
+    public MotionTask(double x, double y) {
         super(x,y,Double.NaN);
     }
 
