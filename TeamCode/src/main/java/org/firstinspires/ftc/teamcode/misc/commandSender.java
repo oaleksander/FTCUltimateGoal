6cf5f71c8533fd86d84ad74d28616f4ad74ed9a9
oaleksander <medvedev.asm@outlook.com>
2020-12-31 21:57:59 +0300

    commandSender class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/commandSender.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/commandSender.java
@@ -2,28 +2,32 @@ package org.firstinspires.ftc.teamcode.misc;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class commandSender {
+import java.util.function.DoubleConsumer;
+
+public class commandSender{
     private double timeout = 1000;
     private double lastValue = Double.NaN;
     private final ElapsedTime lastCommandTimer = new ElapsedTime();
+    DoubleConsumer doubleConsumer;
 
-    public commandSender()
+    public commandSender(DoubleConsumer doubleConsumer)
     {
+        this.doubleConsumer = doubleConsumer;
     }
 
-    public commandSender(double timeout_ms)
+    public commandSender(DoubleConsumer doubleConsumer, double timeout_ms)
     {
+        this.doubleConsumer = doubleConsumer;
         timeout = timeout_ms;
     }
-    public boolean shouldSendCommand(double value)
+    public void sendCommand(double value)
     {
         if(value!=lastValue || lastCommandTimer.milliseconds()>timeout)
         {
+            doubleConsumer.accept(value);
             lastValue = value;
             lastCommandTimer.reset();
-            return true;
         }
-        return false;
     }
 
 }
