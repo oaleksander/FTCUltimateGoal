b4041399e60a4bb836c7bbf7f952ff615bc95290
DmitriiDenisov <denisdmit03@gmail.com>
2021-04-05 13:36:40 +0300

    update VoltageSupplier

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/VoltageSupplier.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/VoltageSupplier.kt
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode.robot
 
 import com.qualcomm.robotcore.hardware.VoltageSensor
+import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.teamcode.superclasses.MultithreadedRobotModule
 import org.firstinspires.ftc.teamcode.superclasses.VoltageSupplier
 
 class VoltageSupplier: MultithreadedRobotModule(), VoltageSupplier{
     private val defaultVoltage = 12.0
     override var voltage: Double = defaultVoltage
+    private val pauseTime = 100.0
+    private val voltageTime = ElapsedTime()
 
     lateinit var voltageSensor: VoltageSensor
 
@@ -15,11 +18,13 @@ class VoltageSupplier: MultithreadedRobotModule(), VoltageSupplier{
     }
 
     override fun updateControlHub() {
-        /*
-        if() ...
-        TODO Rolling average
-        TODO Query timeout
-         */
-        voltage = voltageSensor.voltage
+
+        if(voltageTime.milliseconds() > pauseTime) {
+            voltage = (voltage + voltageSensor.voltage) / 2
+            voltageTime.reset()
+            //TODO Rolling average
+            //TODO Query timeout
+        }
+
     }
 }
